68094814b38984dfcf3f2e80ae5bd4610b18febc
zakaria <zakaria.agoulif@gmail.com>
2013-11-16 14:23:01 +0100

    css feinschliff

--- index.php
+++ index.php
@@ -8,21 +8,22 @@
     </head>
 
     <body>
-        <div id="bg">
         <div id="header">
             <div id="date"><?php echo date("d.m.Y H:m")." Uhr"; ?></div>
-            <div id="user">zaki@gmail.com | logout</div>
+            <div id="user"><a href="#">zaki@gmail.com</a> | <a href="#">logout</a></div>
         </div>
-        <div id="logo"><h1>Musikverein Lotto</h1></div>
-        <div id="notifications">"Biergarten & Lotto" wird gerade gespielt. <a href="/">Hier klicken um in das Spiel zu gelangen.</a></div>
-        <div id="breadcrumbs"><a href="/">Start</a> &gt; Veranstaltung</div>
+        <div id="logo">
+	    <h1><a href="#">Musikverein Lotto</a></h1>
+	</div>
+        <a href="#" class="notification alert">"Biergarten & Lotto" wird gerade gespielt. Hier klicken um in das Spiel zu gelangen.</a>
+        <div class="notification error">Bitte alle Pflichtfelder ausfüllen!</div>
+	<div class="notification successful">Super! Deine Daten sind gespeichert.</div>
+	<div id="breadcrumbs"><a href="/">Start</a> &gt; Veranstaltung</div>
         <div id="content">
             <p>Content</p>
-	    <a href="#" class="buttoncreate">ERSTELLEN</a>
+	    <a href="#" class="button buttoncreate">ERSTELLEN</a>
 	    <a href="#" class="buttonclose">BEENDEN</a>
 	    <a href="#" class="buttonadd">HINZUFÜGEN</a>
         </div>
-        </div>
     </body>
-
 </html>
