afd88963b4e19b2e109c870888aed458c04baa1e
Tobias Scheurer <tobituning@hotmail.com>
2013-11-16 13:45:37 +0100

    created some classes

---
+++ model/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Event
+ *
+ * @author tscheurer
+ */
+class Event {
+    //put your code here
+}
+
+?>
---
+++ model/LottoCard.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LottoCard
+ *
+ * @author tscheurer
+ */
+class LottoCard {
+    //put your code here
+}
+
+?>
